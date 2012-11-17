f624a1a50708aa8d89a0e2b71a6bce777bdf8e65
Peter Law <PeterJCLaw@gmail.com>
2012-11-17 16:45:53 +0000

    Convert to repo(branch) format.
    This includes changing the merged message to look more like the
    others so that they have a consistent feel.

--- gerritbot.py
+++ gerritbot.py
@@ -149,11 +149,11 @@ def change_merged(event):
 
     msg_branch = branch_color + branch + color(GREY)
     msg_project = color(TEAL,bold=True) + project + color(GREY)
-    msg_owner = color(GREEN) + owner + color(GREY)
+    msg_owner = color(GREEN) + owner + "'s" + color(BLACK)
     msg_subject = color() + subject + color(GREY)
     msg_link = color(NAVY, underline=True) + link + color(GREY)
 
-    message = "%s | %s | %s > %s %s" % (msg_branch, msg_project, msg_owner, msg_subject, msg_link)
+    message = "Applied %s change on %s(%s) : %s %s" % (msg_owner, msg_project, msg_branch, msg_subject, msg_link)
     subprocess.call(['./pipebot/say', message])
 
 def comment_added(event):
@@ -178,7 +178,7 @@ def comment_added(event):
     msg_subject = color() + subject + color(GREY)
     msg_link = color(NAVY, underline=True) + link + color(GREY)
 
-    message = "%s reviewed %s | %s : %s %s" % (msg_author, msg_project, msg_branch, msg_subject, msg_link)
+    message = "%s reviewed %s(%s) : %s %s" % (msg_author, msg_project, msg_branch, msg_subject, msg_link)
     subprocess.call(['./pipebot/say', message])
 
 def patchset_created(event):
@@ -204,7 +204,7 @@ def patchset_created(event):
     msg_subject = color() + subject + color(GREY)
     msg_link = color(NAVY, underline=True) + link + color(GREY)
 
-    message = "%s submitted %s | %s : %s %s" % (msg_owner, msg_project, msg_branch, msg_subject, msg_link)
+    message = "%s submitted %s(%s) : %s %s" % (msg_owner, msg_project, msg_branch, msg_subject, msg_link)
     subprocess.call(['./pipebot/say', message])
 
 if __name__ == '__main__':
