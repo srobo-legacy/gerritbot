3155f4a5cd6a59d268a95af1ff26b7737613a528
Alastair Lynn <arplynn@gmail.com>
2012-11-27 14:19:39 +0000

    Port gerritbot to Python 3
    Also institute a number of general tidy-ups.

--- dev.py
+++ dev.py
@@ -1,26 +1,25 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import os
-import simplejson
+import json
 import sys
 
 import gerritbot
 
 if len(sys.argv) < 2:
-    print "Usage: dev.py EVENT_FILE.json [EVENT_FILE.json ..]"
-    print "  Triggers the given events inside gerritbot"
+    print("Usage: dev.py EVENT_FILE.json [EVENT_FILE.json ..]",
+          file=sys.stderr)
+    print("  Triggers the given events inside gerritbot", file=sys.stderr)
     exit(1)
 
 paths = sys.argv[1:]
-for filePath in paths:
-    if not os.path.exists(filePath):
-        print "File '%s' doesn't exist." % filePath
+for file_path in paths:
+    if not os.path.exists(file_path):
+        print("File '{0}' doesn't exist.".format(file_path), file=sys.stderr)
         continue
 
     event = None
-    with open(filePath) as f:
-        event_data = f.read()
-
-    event = simplejson.loads(event_data)
+    with open(file_path, 'r') as f:
+        event = json.load(f)
 
     gerritbot.trigger(event)
