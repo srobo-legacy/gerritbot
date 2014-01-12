d2c3c6b69aca47e394bb824cf6da7905c94b627e
Peter Law <PeterJCLaw@gmail.com>
2014-01-12 17:31:43 +0000

    Introduce a subscription model for event handlers.
    Having each handler subscribe to events via a decorator makes it
    clearer from the handlers which each one is used for. Move the
    handlers and routing functionality into their own files, now that
    it is convenient to do so.
    
    This also makes it very easy to add more handlers, and further provides
    separation between each of the parts of the system.
    
    Change-Id: If49d81953dbac9a44110034db711634fbd47b3eb

---
+++ routing.py
@@ -0,0 +1,17 @@
+
+_handlers = {}
+
+def trigger(event):
+    event_type = event['type']
+    if event_type in _handlers:
+        for handler in _handlers[event_type]:
+            handler(event)
+    else:
+        print "Unhandled event type '{0}'.".format(event_type)
+
+def register_for(event_type):
+    def wrapper(handler):
+        if not event_type in _handlers:
+            _handlers[event_type] = []
+        _handlers[event_type].append(handler)
+    return wrapper
